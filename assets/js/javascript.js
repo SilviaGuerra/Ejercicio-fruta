82a5d4768b97add7c8314830953cc33a21c1f1cc
SilviaGuerra <silvia.guerrahdz@gmail.com>
2017-03-20 23:37:58 -0600

    Ejercicio fruta1

---
+++ assets/js/javascript.js
@@ -0,0 +1,8 @@
+function agregarFruta (){
+  var catalogo = ["Manzana", "Pera", "Piña", "Cereza", "Naranja"];
+  var fruta = document.getElementById("fruta").value;
+  var nuevaFruta = document.getElementById("nuevaFruta");
+  if(nuevoElemento == true){
+    catalogo.push(fruta);
+  }
+}
